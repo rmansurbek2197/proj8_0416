cf8e4f9de7d99c33962aa67ef47ed11c0df5ee84
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-15 23:43:47 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# 81
+nums=[1,2,3]
+nums.insert(1,100)
+print(nums)
+
+# 82
+nums=[1,2,3]
+nums.remove(2)
+print(nums)
+
+# 83
+text="abc"
+for i,c in enumerate(text):
+    print(i,c)
+
+# 84
+nums=[1,2,3]
+print(any(x>2 for x in nums))
+
+# 85
+nums=[1,2,3]
+print(all(x>0 for x in nums))
+
+# 86
+print(sorted([3,1,2]))
+
+# 87
+print(list(reversed([1,2,3])))
+
+# 88
+print(abs(-10))
+
+# 89
+print(pow(2,5))
+
+# 90
+print(divmod(10,3))
